46c77c2ecd7f080626c20572dc773f6ae38c57e3
Vasileios Ntoufoudis <info@ntoufoudis.com>
2025-02-10 18:43:33 +0200

    Update Configuration

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -3,6 +3,7 @@ import { defineUserConfig } from 'vuepress/cli'
 import { viteBundler } from '@vuepress/bundler-vite'
 
 export default defineUserConfig({
+  base: '/novapress_doc/',
   lang: 'en-US',
 
   title: 'VuePress',
